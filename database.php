20cef5ad7239bd22d9d02212dc14ebffc25e9d4e
Alix Most <alsaechao@mail.fhsu.edu>
2021-03-05 13:31:38 -0600

    test

--- database.php
+++ database.php
@@ -1,5 +1,5 @@
 <?php 
-    $dsn = 'mysql:host=localhost;dbname=zippysusedautos';
+   /* $dsn = 'mysql:host=localhost;dbname=zippyusedautos';
     $username = 'root';
 
 
@@ -10,5 +10,21 @@
         echo $error_message;
         exit();
     }
+*/
+    
 
-    ?>
+ 
+    $dsn = 'mysql:lyn7gfxo996yjjco.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=patsjzwz4qgedw8r';
+    $username = 'f7hh5s22ggfpu19t';
+    $password = 'ajvuqgzttv0vh7kc';
+
+
+    try {
+        $db = new PDO($dsn, $username);
+    } catch (PDOException $e) {
+        $error_message = $e -> getMessage();
+        echo $error_message;
+        exit();
+    }
+
+    ?> 
